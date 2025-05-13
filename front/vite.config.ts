05d04af365a75f551a855e80d18fb8d84c92d5b5
Ivan Konorkin <ivan@intelas.com>
2025-05-13 11:17:17 +0100

    front perfomance improvements

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,6 +1,22 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import viteCompression from 'vite-plugin-compression';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+
+		viteCompression({
+			algorithm: 'gzip',
+			ext: '.gz',
+			threshold: 10240,
+			deleteOriginFile: false
+		}),
+
+		viteCompression({
+			algorithm: 'brotliCompress',
+			ext: '.br',
+			threshold: 10240
+		})
+	],
 });
